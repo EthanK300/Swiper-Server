0930d002d868d39b4651e26cce60c91e487b74c7
Ethan <1bluejellybean@gmail.com>
2025-07-19 00:28:04 -0400

    remove extra c

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 
     return (
         <footer className="footer">
-            <p>&copy; 2025 © Swiper by Ethan K. All rights reserved</p>
+            <p>&copy; 2025 Swiper by Ethan K. All rights reserved</p>
         </footer>
     );
 }
