9b0fdb3a52b70acdf06a5440e735bf9a03f6520e
Ethan <1bluejellybean@gmail.com>
2025-08-01 03:51:28 -0400

    add frontend form validation to signup/login

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,24 +1,55 @@
+import React from "react";
+import { useForm } from "react-hook-form";
 import { useState } from 'react';
 import "../styles/signup-form.css";
-
-const loginGuest = () => {
-
-};
 
 function SignupForm() {
 
     const [register, setRegister] = useState(false);
+
+    const loginGuest = () => {
+        console.log("log in as guest");
+    };
+
+    const {
+        register: registerLogin,
+        handleSubmit: handleLoginSubmit,
+        formState: { errors: loginErrors },
+    } = useForm();
+
+    const {
+        register: registerRegister,
+        handleSubmit: handleRegisterSubmit,
+        watch: watchRegister,
+        formState: { errors: registerErrors },
+    } = useForm();
+
+    const onLogin = (data) => {
+        console.log("test login");
+        console.log(data);
+    };
+
+    const onRegister = (data) => {
+        console.log("test register");
+        console.log(data);
+    }
+
+    const password = watchRegister("password", "");
 
     return(
         <div id="form-container">
             <div id="slider" style={{ transform: `translateX(${register ? '-50%' : '0'})` }}>
                 <div id="login" class="panel">  
                     <h2>Login</h2>
-                    <form>
-                        <label for="emailuser">Email/Username</label>
-                        <input type="email" placeholder="eg: jeff@gmail.com" name="emailuser" required/>
+                    <form onSubmit={handleLoginSubmit(onLogin)} noValidate>
+                        <label for="lemail">Email/Username</label>
+                        <input type="email" placeholder="eg: jeff@gmail.com" id="lemail" {...registerLogin("email", { required: "Email is required"})}/>
+                        {loginErrors.email && <p class="error">{loginErrors.email.message}</p>}
+
                         <label for="lpassword">Password</label>
-                        <input type="password" placeholder="eg: jeff999iscool" name="lassword" id="lpassword" required/>
+                        <input type="password" placeholder="eg: jeff999iscool" id="lpassword" {...registerLogin("password", { required: "Password is required"})}/>
+                        {loginErrors.password && <p class="error">{loginErrors.password.message}</p>}
+
                         <button type="submit">Login</button>
                     </form>
                     <p>
@@ -30,15 +61,25 @@ function SignupForm() {
                 </div>
                 <div id="register" class="panel">
                     <h2>Register</h2>
-                    <form>
+                    <form onSubmit={handleRegisterSubmit(onRegister)} noValidate>
                         <label for="rname">Name</label>
-                        <input type="text" placeholder="eg: Jeff" name="rname" id="rname" required/>
+                        <input type="text" placeholder="eg: Jeff" id="rname" {...registerRegister("name", { required: "Must have a name"})}/>
+                        {registerErrors.name && <p class="error">{registerErrors.name.message}</p>}
+
                         <label for="remail">Email</label>
-                        <input type="email" placeholder="eg: jeff@gmail.com" name="remail" id="remail"/>
+                        <input type="email" placeholder="eg: jeff@gmail.com" id="remail" {...registerRegister("email", { pattern: { value: /^\S+@\S+\.\S+$/, message: "Invalid email format"}})}/>
+                        {registerErrors.email && <p class="error">{registerErrors.email.message}</p>}
+                        
                         <label for="rpassword">Password</label>
-                        <input type="password" placeholder="eg: jeff999iscool" name="rpassword" id="rpassword" required/>
+                        <input type="password" placeholder="eg: jeff999iscool" id="rpassword" {...registerRegister("password", { required: "Password is required"})}/>
+                        {registerErrors.password && <p class="error">{registerErrors.password.message}</p>}
+                        
                         <label for="rcpassword">Confirm Password</label>
-                        <input type="password" name="rcpassword" id="rcpassword" required/>
+                        <input type="password" id="rcpassword" {...registerRegister("cpassword", { required: "Password confirmation required", validate: (value) =>
+                            value === password || "Passwords do not match"
+                        })}/>
+                        {registerErrors.cpassword && <p class="error">{registerErrors.cpassword.message}</p>}
+                        
                         <button type="submit">Register</button>
                     </form>
                     <p>
