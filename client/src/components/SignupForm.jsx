9a4fa2f5a962f4207118544184ab6f3fb10d111f
Ethan <1bluejellybean@gmail.com>
2025-07-31 22:17:35 -0400

    signin form work & styling

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -11,31 +11,39 @@ function SignupForm() {
 
     return(
         <div id="form-container">
-            <div id="slider" style={{ transform: `translateX(${register ? '-100%' : '0'})` }}>
-                <div id="login" class="panel">
+            <div id="slider" style={{ transform: `translateX(${register ? '-50%' : '0'})` }}>
+                <div id="login" class="panel">  
                     <h2>Login</h2>
                     <form>
-                        <input type="email" placeholder="Email/Username"/>
-                        <input type="password" placeholder="Password"/>
+                        <label for="emailuser">Email/Username</label>
+                        <input type="email" placeholder="eg: jeff@gmail.com" name="emailuser" required/>
+                        <label for="lpassword">Password</label>
+                        <input type="password" placeholder="eg: jeff999iscool" name="lassword" id="lpassword" required/>
                         <button type="submit">Login</button>
                     </form>
                     <p>
                         Don't have an account?
+                        <br/>
                         <button onClick={() => setRegister(true)}>Register</button>
+                        <button onClick={loginGuest}>Login as a guest</button>
                     </p>
-                    <button onClick={loginGuest}>Login as a guest</button>
                 </div>
                 <div id="register" class="panel">
                     <h2>Register</h2>
                     <form>
-                        <input type="text" placeholder="Name"/>
-                        <input type="email" placeholder="Email"/>
-                        <input type="password" placeholder="Password"/>
-                        <input type="password" placeholder="Confirm Password"/>
+                        <label for="rname">Name</label>
+                        <input type="text" placeholder="eg: Jeff" name="rname" id="rname" required/>
+                        <label for="remail">Email</label>
+                        <input type="email" placeholder="eg: jeff@gmail.com" name="remail" id="remail"/>
+                        <label for="rpassword">Password</label>
+                        <input type="password" placeholder="eg: jeff999iscool" name="rpassword" id="rpassword" required/>
+                        <label for="rcpassword">Confirm Password</label>
+                        <input type="password" name="rcpassword" id="rcpassword" required/>
                         <button type="submit">Register</button>
                     </form>
                     <p>
                         Already have an account?
+                        <br/>
                         <button onClick={() => setRegister(false)}>Login</button>
                     </p>
                 </div>
